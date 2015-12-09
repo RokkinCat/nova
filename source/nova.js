e88ebd9a845408707c918116a56602d73a08e056
Rick Allen <rick@rokkincat.com>
2015-12-09 14:17:49 -0600

    More placeholder methods

--- source/nova.js
+++ source/nova.js
@@ -1,7 +1,35 @@
-var Nova = NovaObject.extend({
-	'Object': NovaObject,
-	'Model': NovaModel,
-	'Collection': NovaCollection,
-	'View': NovaView,
-	'Component': NovaComponent
-});
+var Nova = NovaObject.extend( (function() {
+	var applications = {},
+	    currentApplication = null,
+	    currentObject = null;
+
+	return {
+		application: function(name) {
+			return this;
+		},
+
+		model: function(name) {
+			return this;
+		},
+
+		collection: function(name) {
+			return this;
+		},
+
+		view: function(name) {
+			return this;
+		},
+
+		component: function(name) {
+			return this;
+		},
+
+		imports: function(name) {
+			return this;
+		},
+
+		definition: function(baseClassName, generator) {
+			return this;
+		};
+	};
+}; )() );
